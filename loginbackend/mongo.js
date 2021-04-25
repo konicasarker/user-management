1dfb9a11999bf73167c8e420f3c725a6cba8a275
konica.sarker <konica.sarker@bonial.com>
2021-04-26 01:17:03 +0200

    added env variable

--- loginbackend/mongo.js
+++ loginbackend/mongo.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoPath = REACT_APP_DATABASE_ACCESS
 
+// check this
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useNewUrlParser: true,
