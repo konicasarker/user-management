d321d29c67fb889027d8a5d3803aab782eef7294
konica.sarker <konica.sarker@bonial.com>
2021-04-26 01:27:56 +0200

    update

--- loginbackend/mongo.js
+++ loginbackend/mongo.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const mongoPath = '';
-
+const mongoPath = REACT_APP_DATABASE_ACCESS
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
